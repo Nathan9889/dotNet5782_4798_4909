962f1bd947363741e52979eba5ec4a9e2516ff1d
Nathan9889 <nsaigh@g.jct.ac.il>
2021-10-06 23:07:57 +0300

    Initial code + Welcome functions

---
+++ Targil0/Program4798.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4798();
+            Welcome4909();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome4909();
+
+        private static void Welcome4798()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName;
+            userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
